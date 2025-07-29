7f685ddbb6a4af523f3f1835c1bb3eca8e347215
Guilherme <guilherme.s.barros@outlook.com>
2025-07-28 21:58:38 -0300

    feat: header component

---
+++ src/components/header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  padding: 4rem 0 8.375rem;
+
+  background: url("/public/cover.svg") center center;
+  background-size: cover;
+`
--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '../../components/header'
+
 export function DefaultLayout() {
 	return (
 		<>
-			<header>Header</header>
+			<Header />
 
 			<div>
 				<Outlet />
